5bf5afa7050abb5e8eacec1b922cb1c73f55ffbe
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-11-06 12:08:29 +0000

    Add a health check endpoint

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Certification Management System!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'Healthy' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
